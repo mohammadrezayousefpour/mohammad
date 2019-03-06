efd2ed94c09254bac9eab266f36a8ee46afd9ff1
lenovo <mohammad0reza79@gmail.com>
2019-03-06 16:36:38 +0330

    change main file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int x=5;
+        int y=5;
+        System.out.println(x+y);
     }
 }
